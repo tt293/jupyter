419e394fcf43939d4b0cc9c324af0b01c05b5f10
Thomas <thomas.trenner@gmail.com>
2017-10-09 13:08:00 +0100

    Moving common code to shared python module, adding stopping of instance to shell script and finalizing notebook.

--- aws_scripts/aws-start.py
+++ aws_scripts/aws-start.py
@@ -1,5 +1,6 @@
 import boto3
 import sys
+import aws_common
 
 
 def get_instance_info(ec2_client):
@@ -10,29 +11,11 @@ def get_instance_info(ec2_client):
             for instance in instances}
 
 
-def start_instance(ec2_client, name):
-    from time import sleep
-    instance_info = get_instance_info(ec2_client)
-    response = ec2_client.start_instances(
-        InstanceIds=[instance_info[name]['InstanceId']])
-    si = response['StartingInstances']
-    states = ['running', 'pending']
-    if len(si) != 1 or si[0]['CurrentState']['Name'] not in states:
-        print("Something went wrong!", response)
-        return ""
-    else:
-        while True:
-            sleep(0.5)
-            dns_name = get_instance_info(ec2_client)[name]['PublicDnsName']
-            if dns_name:
-                return dns_name
-
-
 def main():
     instance_name = sys.argv[1]
     region_name = sys.argv[2] if len(sys.argv) > 2 else 'us-west-2'
     ec2 = boto3.client('ec2', region_name=region_name)
-    print(start_instance(ec2, instance_name))
+    print(aws_common.start_instance(ec2, instance_name))
 
 
 if __name__ == '__main__':
--- aws_scripts/start_it.sh
+++ aws_scripts/start_it.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pubDNSName=$(python3 aws-start.py $1)
-ssh -i /mnt/c/temp/MyJupyter.pem -L 8000:localhost:8888 -o "StrictHostKeyChecking=no" -l ubuntu $pubDNSName
-stopping=$(python3 aws-stop.py $1)
+pubDNSName=$(python3 aws-start.py $2 $3)
+ssh -i $1 -L 8000:localhost:8888 -o "StrictHostKeyChecking=no" -l ubuntu $pubDNSName
+stopping=$(python3 aws-stop.py $2)
 echo $stopping
