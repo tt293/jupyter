050554a8fad342093ebf0258a349608f67f78b80
Thomas <thomas.trenner@gmail.com>
2017-10-09 13:21:29 +0100

    Remove get_instance_info from aws_start

--- aws_scripts/aws-start.py
+++ aws_scripts/aws-start.py
@@ -3,14 +3,6 @@ import sys
 import aws_common
 
 
-def get_instance_info(ec2_client):
-    response = ec2_client.describe_instances()
-    instances = [x for r in response['Reservations'] for x in r['Instances']]
-    keys = ['InstanceId', 'InstanceType', 'State', 'PublicDnsName']
-    return {instance['Tags'][0]['Value']: {k: instance[k] for k in keys}
-            for instance in instances}
-
-
 def main():
     instance_name = sys.argv[1]
     region_name = sys.argv[2] if len(sys.argv) > 2 else 'us-west-2'
